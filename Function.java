cf2e59332fbde2adef0c8ced28da36eb1251ea1f
garima2004yadav <115277218+garima2004yadav@users.noreply.github.com>
2023-10-17 21:41:36 +0530

    Add files via upload

---
+++ Function.java
@@ -0,0 +1,12 @@
+public class Function { 
+        static int sum(int a,int b){
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = sum(a , b);
+        System.out.println("The sum is "+c);
+    }
+    
+}
